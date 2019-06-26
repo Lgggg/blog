eccc9edf3a1195b8708be59fb830964d8f1a0517
jiuluncai <jiuluncai@tencent.com>
2019-06-27 01:25:24 +0800

    finish 8

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
 import main
 import models
 
 manager = Manager(main.app)
+migrate = Migrate(main.app, models.db)
 
 manager.add_command("server", Server(host='0.0.0.0', port=8080))
+manager.add_command("db", MigrateCommand)
 
 @manager.shell
 def make_shell_context():
@@ -17,6 +21,8 @@ def make_shell_context():
             db   = models.db,
             User = models.User,
             Post = models.Post,
+            Comment = models.Comment,
+            Tag = models.Tag
     )
 
 if __name__ == '__main__':
---
+++ op.py
@@ -0,0 +1,67 @@
+==================================【6】==================================
+#(M)VC_models 的关系(one to many)
+=========【manager shell】=========
+from uuid import uuid4
+user = User(id=str(uuid4()), username='jmilkfan', password='fanguiju')
+db.session.add(user)
+db.session.commit()
+user.posts
+
+user.posts.all()
+post_one = Post('First Post')
+post_one.id = str(uuid4())
+post_one.user_id = user.id
+db.session.add(post_one)
+db.session.commit()
+user.posts.all()
+user = db.session.query(User).first()
+user.id
+post_second = Post('Second Post')
+post_second.id = str(uuid4())
+post_second.users
+post_second.users = user
+db.session.add(post_second)
+db.session.commit()
+
+
+==================================【7】==================================
+#(M)VC_models 的关系(many to many)
+=========【manager shell】=========
+from uuid import uuid4
+posts = db.session.query(Post).all()
+posts
+post_one = posts[1]
+post_two = posts[0]
+from uuid import uuid4
+tag_one = Tag('JmilkFan')
+tag_one.id = str(uuid4())
+tag_two = Tag('FanGuiju')
+tag_two.id = str(uuid4())
+tag_three = Tag('Flask')
+tag_three.id = str(uuid4())
+post_one.tags
+post_one.tags = [tag_two]
+post_two.tags = [tag_one, tag_two, tag_three]
+db.session.add(post_one)
+db.session.add(post_two)
+db.session.commit()
+
+tag_one.posts.all()
+tag_one.posts.append(post_one)
+tag_one.posts.all()
+post_one.tags
+#A+B | A.B->list| B.A->AppenderBaseQuery
+db.session.add(tag_one)
+db.session.commit()
+
+db.session.query(Post).filter_by(title='First Post').first().tags
+db.session.query(Tag).filter_by(name='JmilkFan').first().posts.all()
+
+
+==================================【8】==================================
+#(M)VC_models 的关系(many to many)
+=========【pip】=========
+pip install Flask-Migrate
+pip freeze > requirements.txt
+
+=========【manager shell】=========
